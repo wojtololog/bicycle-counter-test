fc4866ea2fb6c7f5e9f51313c6f8fd5b0d73ea96
Wojto <wojtololog@gmail.com>
2018-10-11 01:02:28 +0200

    PermissionProcessor created - class which grants GPS permission; if user deny permission it shows information why he should grant permission

--- app/src/main/java/com/intern/wlacheta/testapp/MainActivity.java
+++ app/src/main/java/com/intern/wlacheta/testapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.intern.wlacheta.testapp;
 
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.intern.wlacheta.testapp.permissions.PermissionsProcessor;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private PermissionsProcessor permissionsProcessor = new PermissionsProcessor(this,this);
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!permissionsProcessor.isPermissionsGranted()) {
+            permissionsProcessor.requestStoragePermission();
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == permissionsProcessor.getLocationAllPermissions())  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/intern/wlacheta/testapp/permissions/PermissionsProcessor.java
@@ -0,0 +1,61 @@
+package com.intern.wlacheta.testapp.permissions;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+public class PermissionsProcessor {
+    private final int LOCATION_ALL_PERMISSIONS = 1;
+    private final String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    private Context context;
+    private Activity activity;
+
+    public PermissionsProcessor(Context passedContext, Activity passedActivity) {
+        this.context = passedContext;
+        this.activity = passedActivity;
+    }
+
+    public boolean isPermissionsGranted() {
+        if (ContextCompat.checkSelfPermission(context, PERMISSIONS[0]) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(context, PERMISSIONS[1]) == PackageManager.PERMISSION_GRANTED ) {
+            Toast.makeText(context, "You have already granted geolocation permission!",
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, PERMISSIONS[0]) || ActivityCompat.shouldShowRequestPermissionRationale(activity, PERMISSIONS[1])) {
+            new AlertDialog.Builder(context)
+                    .setTitle("Permission needed")
+                    .setMessage("This app requires GPS to work correctly")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(activity, PERMISSIONS, LOCATION_ALL_PERMISSIONS);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(activity, PERMISSIONS, LOCATION_ALL_PERMISSIONS);
+        }
+    }
+
+    public int getLocationAllPermissions() {
+        return LOCATION_ALL_PERMISSIONS;
+    }
+}
