08ae800651580baea9e88c7295f3da23adbc522e
Wojto <wlacheta@interia.pl>
2018-10-20 19:02:40 +0200

    Switching between tabs

--- app/src/main/java/com/intern/wlacheta/testapp/activites/MainActivity.java
+++ app/src/main/java/com/intern/wlacheta/testapp/activites/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.intern.wlacheta.testapp.R;
+import com.intern.wlacheta.testapp.gesture.PageAdaptor;
 import com.intern.wlacheta.testapp.location.LocationTracker;
 import com.intern.wlacheta.testapp.permissions.PermissionsProcessor;
 
@@ -22,17 +24,46 @@ public class MainActivity extends AppCompatActivity {
     private final PermissionsProcessor permissionsProcessor = new PermissionsProcessor(this,this);
 
     private Button startButton, stopButton;
+    private Toolbar toolbar;
+
+    private TabLayout tabLayout;
+    private TabItem tabTracker, tabTrips;
+
+    private PageAdaptor pageAdaptor;
+    private ViewPager viewPager;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.app_name));
-        TabLayout tabLayout = findViewById(R.id.tablayout);
-        TabItem tabTracker = findViewById(R.id.tabTracker);
-        TabItem tabTrips = findViewById(R.id.tabTrips);
+        tabLayout = findViewById(R.id.tabLayout);
+        tabTracker = findViewById(R.id.tabTracker);
+        tabTrips = findViewById(R.id.tabTrips);
+        viewPager = findViewById(R.id.viewPager);
+
+        pageAdaptor = new PageAdaptor(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdaptor);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
         if(permissionsProcessor.isPermissionsNotGranted()) {
             permissionsProcessor.requestLocationPermissions();
