09c450d8929a9ffcbdd262901ea1df046743fcb9
Wojto <wlacheta@interia.pl>
2018-10-20 17:48:57 +0200

    PageAdaptor -> handles horizontal gesture between tabs

---
+++ app/src/main/java/com/intern/wlacheta/testapp/gesture/PageAdaptor.java
@@ -0,0 +1,22 @@
+package com.intern.wlacheta.testapp.gesture;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PageAdaptor extends FragmentPagerAdapter {
+
+    public PageAdaptor(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
