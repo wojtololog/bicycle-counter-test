5ed81c7d4a21b84624bc038d37c760ea3423b794
Wojto <wojtololog@gmail.com>
2018-10-22 21:54:50 +0200

    Simple implementation of adding to database in activity NewDBElementActivity; RecyclerView implementation based on WordListAdapter

--- app/src/main/java/com/intern/wlacheta/testapp/activities/TripsActivity.java
+++ app/src/main/java/com/intern/wlacheta/testapp/activities/TripsActivity.java
@@ -1,20 +1,57 @@
 package com.intern.wlacheta.testapp.activities;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.intern.wlacheta.testapp.R;
+import com.intern.wlacheta.testapp.database.WordListAdapter;
+import com.intern.wlacheta.testapp.database.entities.Word;
+import com.intern.wlacheta.testapp.database.viewmodel.WordsViewModel;
+
+import java.util.List;
 
 public class TripsActivity extends AppCompatActivity {
+    private static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    private WordsViewModel wordsViewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trips);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        wordsViewModel = ViewModelProviders.of(this).get(WordsViewModel.class);
+        wordsViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                adapter.setWords(words);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.addToDB);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TripsActivity.this, NewDBElementActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
     }
 
     @Override
@@ -52,5 +89,19 @@ public class TripsActivity extends AppCompatActivity {
     private void showTracker() {
         Intent intent = new Intent(this, TrackerActivity.class);
         startActivity(intent);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewDBElementActivity.EXTRA_REPLY));
+            wordsViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
