aabec9a2f787ec61a45afd55b4f5c7667d1bbf4f
Wojto <wojtololog@gmail.com>
2018-10-22 15:44:20 +0200

    AndroidManifest -> change launch mode of activities to singleton; even if try to create new one firstly
    it is checked if activity exists. Menu in TripsActivity

--- app/src/main/java/com/intern/wlacheta/testapp/activities/TripsActivity.java
+++ app/src/main/java/com/intern/wlacheta/testapp/activities/TripsActivity.java
@@ -1,8 +1,12 @@
 package com.intern.wlacheta.testapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.intern.wlacheta.testapp.R;
 
@@ -11,5 +15,42 @@ public class TripsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trips);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.trips_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.GPS_trips:
+                showTracker();
+                return true;
+            case R.id.settings_trips:
+                showSettings();
+                return true;
+            case R.id.help_trips:
+                showHelp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showHelp() {
+
+    }
+
+    private void showSettings() {
+
+    }
+
+    private void showTracker() {
+        Intent intent = new Intent(this, TrackerActivity.class);
+        startActivity(intent);
     }
 }
